60bd1260bba3b0fd74bcc32644bba8e521c027c7
Jens Walrave <JKWalrave@users.noreply.github.com>
2021-10-26 14:52:53 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:4.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
@@ -17,5 +17,5 @@ let package = Package(
         )
     ],
 
-    swiftLanguageVersions: [.v4]
+    swiftLanguageVersions: [.v4_2]
 )
